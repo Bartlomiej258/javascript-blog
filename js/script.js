3d6b55782ddc34007e5aaa31e370f7f69019c2aa
Bartlomiej Lorenowicz <barteklorenowicz258@gmail.com>
2021-10-07 18:41:38 +0200

    add active class onClick

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 const titleClickHandler = function(event) {
+  const clickedElement = this;
   console.log('Link was clicked!');
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
 for(let activeLink of activeLinks) {
   activeLink.classList.remove('active');
 }
 
-  /* add class 'active' to the clicked link */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+  clickedElement.classList.add('active');
+  console.log('clickedElement:', clickedElement);
 
-  /* remove class 'active' from all articles */
+  /* [DONE] remove class 'active' from all articles */
   const activeArticleLinks = document.querySelectorAll('.posts .active');
 
   for(let activeArticleLink of activeArticleLinks) {
