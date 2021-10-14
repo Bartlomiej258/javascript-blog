6149e931617c8764ad669521b69355602081ced4
Bartlomiej Lorenowicz <barteklorenowicz258@gmail.com>
2021-10-14 23:28:16 +0200

    generate links list of titles

--- js/script.js
+++ js/script.js
@@ -28,13 +28,52 @@ for(let activeLink of activeLinks) {
   /* find the correct article using the selector (value of 'href' attribute) */
   const targetArticle = document.querySelector(articleSelector);
   console.log('targetArticle', targetArticle);
+
   /* add class 'active' to the correct article */
   targetArticle.classList.add('active');
   console.log('targetArticle', targetArticle);
-}
+
 
 const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
+}
+  const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+  
+function generateTitleLinks(){
+console.log('execution of generateTitleLinks')
+  // remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+  
+  // for each article 
+  const articles = document.querySelectorAll(optArticleSelector);
+  for(let article of articles) {
+  
+    /* get the article id */
+  const articleId = article.getAttribute('id');
+  console.log(articleId);  
+  
+    /* find the title element */
+    /* get the title from the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+  
+    /* create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log('link html');
+  
+      
+    /* insert link into titleList */
+    titleList.innerHTML = titleList.innerHTML + linkHTML;
+    console.log('html');
+}
+  titleList.innerHTML = html;
+}
+
+generateTitleLinks();
+
 }
